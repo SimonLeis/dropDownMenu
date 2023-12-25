38d7a16d405ec0051c1eed0550082c02f403d1fc
SimonLeis <145809981+SimonLeis@users.noreply.github.com>
2023-12-25 19:45:40 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { SourceMap } = require("module");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "source-map",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "title",
+      filename: "index.html",
+      inject: "body",
+      template: "./src/index.html",
+    }),
+  ],
+};
